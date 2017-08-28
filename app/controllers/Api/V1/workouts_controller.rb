bf03a0051cfb8645a99df5b3775fb3a7d00e5371
Jexeones24 <jexeones@gmail.com>
2017-08-28 13:44:24 -0400

    creates full workout with movements, style, reps

--- app/controllers/Api/V1/workouts_controller.rb
+++ app/controllers/Api/V1/workouts_controller.rb
@@ -1,39 +1,40 @@
 class Api::V1::WorkoutsController < ApplicationController
 
   def index
-  workouts = Workout.all
-  render json: workouts
-end
+    workouts = Workout.all
+    render json: workouts
+  end
 
-def create
-  workout = Workout.create(workout_params)
-  current_user.workouts << workout
-  render json: workout
-end
+  def create
+    workout = Workout.create(workout_params)
+    current_user.workouts << workout
+    render json: workout
+  end
 
-def show
-  workout = Workout.find_by(id: params[:id])
-  render json: workout
-end
+  def show
+    workout = Workout.find_by(id: params[:id])
+    render json: workout
+  end
 
-def update
-  # byebug
-  workout = Workout.find_by(id: params[:id])
-  workout.update(workout_params)
-  render json: workout
-end
+  def update
+    # byebug
+    workout = Workout.find_by(id: params[:id])
+    workout.update(workout_params)
+    render json: workout
+  end
 
-def destroy
-  # byebug
-  workout = Workout.find_by(id: params[:id])
-  workout.destroy
-  workouts = Workout.all
-  render json: workouts
-end
+  def destroy
+    # byebug
+    workout = Workout.find_by(id: params[:id])
+    workout.destroy
+    workouts = Workout.all
+    render json: workouts
+  end
 
-private
+  private
 
-def workout_params
-  params.require(:workout).permit(:name, :user_id)
-end
+  def workout_params
+    params.require(:workout).permit(:name, :style, :chosenMovements, :repsPer, :user_id)
+  end
+
 end
